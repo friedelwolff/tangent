824f4fe97289e0fe3b090af2a2ad313ecd6a3200
Friedel Wolff <friedel@translate.org.za>
2019-05-03 20:42:05 +0200

    First basic tests

---
+++ employees/tests/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase, override_settings
+from django.test.client import Client
+
+
+class TestURLS(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Log in")
+
+    def test_login(self):
+        response = self.client.get('/auth/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Username")
+
+        response = self.client.post('/auth/login/', follow=True, data={
+            "username": "pravin.gordhan",
+            "password": "pravin.gordhan",
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Log in")
+        self.assertContains(response, "Log out")
