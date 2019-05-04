8faefcee4abc30b847ecc09596a970ba2314c463
Friedel Wolff <friedel@translate.org.za>
2019-05-04 21:20:14 +0200

    +Helper method for API calls
    This keeps error handling and headers in one place to avoid duplication.

--- employees/api.py
+++ employees/api.py
@@ -24,6 +24,16 @@ class WebAPIClient:
     def from_request(cls, request):
         return cls(request.session['api_token'])
 
+    def _api_helper(self, endpoint):
+        #TODO: cache based on token and endpoint
+        headers = {"Authorization": "Token %s" % self.token}
+        try:
+            r = requests.get(settings.API_URL + endpoint, headers=headers)
+            r.raise_for_status()
+            return r.json()
+        except (requests.exceptions.RequestException, ValueError) as e:
+            raise WebClientError() from e
+
 
 if settings.MOCK_API:
     from employees.tests.mock_api import WebAPIClient
