52c988797972e7aac69dcc73dd28fcbcbf86ec67
Friedel Wolff <friedel@translate.org.za>
2019-05-07 23:28:13 +0200

    Factor out abbreviated choices
    This will allow reuse.

--- employees/forms.py
+++ employees/forms.py
@@ -1,6 +1,27 @@
 from django import forms
 
 
+GENDERS = {
+        'M': 'Male',
+        'F': 'Female',
+}
+
+RACES = {
+        'B': 'Black',
+        'C': 'Coloured',
+        'I': 'Indian or Asian',
+        'W': 'White',
+        'N': 'None dominant',
+}
+
+REVIEW_TYPES = {
+        'P': 'Performance increase',
+        'S': 'Starting salary',
+        'A': 'Anual increase',
+        'E': 'Expectation review',
+}
+
+
 class SearchForm(forms.Form):
     email__contains = forms.CharField(required=False)
     position = forms.ChoiceField(required=False, choices=(
@@ -11,15 +32,6 @@ class SearchForm(forms.Form):
         ('4', 'Projct Manager (Junior)'),
     ))
     gender = forms.ChoiceField(required=False, choices=(
-        (None, ''),
-        ('M', 'Male'),
-        ('F', 'Female'),
-    ))
+        (None, ''),) + tuple(GENDERS.items()))
     race = forms.ChoiceField(required=False, choices=(
-        (None, ''),
-        ('B', 'Black'),
-        ('C', 'Coloured'),
-        ('I', 'Indian or Asian'),
-        ('W', 'White'),
-        ('N', 'None dominant'),
-    ))
+        (None, ''),) + tuple(RACES.items()))
